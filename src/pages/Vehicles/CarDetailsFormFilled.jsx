052c2e73e09d72276400cb2b09b6c18e66373b20
nde booy <ndesimoniche@gmail.com>
2024-08-18 11:22:50 +0400

    fixed all the errors

--- src/pages/Vehicles/CarDetailsFormFilled.jsx
+++ src/pages/Vehicles/CarDetailsFormFilled.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { ItemStyle, } from "../../components/Form/reservationForm/ReservationDropdown";
 
 
-import { ItemStyle } from "../../components/Form/reservationForm/ReservationDropdown";
 
 export const UserDetailStyle = styled.span({
     textTransform: "capitalize",
@@ -25,28 +24,7 @@ export const CarInfoDetails = styled.div({
     fontFamily: "roboto",
 });
 
-<<<<<<< HEAD
-const CarDetailsFormFilled = ({ carDetails }) => {
-    //now we need the users details and we just immediately destructure them
-    const { setIsLoading } = useMyContext();
-    const navigate = useNavigate();
 
-    const submitForm = (e) => {
-        e.preventDefault();
-        bookAndPayLatterNotFillAtStart(carDetails, setIsLoading, navigate);
-        console.log("yooooo man ");
-    };
-
-    return (
-        <div className="border p-3">
-            <h1 className="font-extrabold text-3xl pb-5 capitalize p-2">
-                make reservation for : {carDetails.name}
-            </h1>
-            <h2 className="px-3 capitalize font-bold text-red-600">
-                please check if your booking information is correct ?
-            </h2>
-
-=======
 
 const CarDetailsFormFilled = ({ carDetails }) => {
     //now we need the users details and we just immediately destructure them
@@ -67,7 +45,7 @@ const CarDetailsFormFilled = ({ carDetails }) => {
                 please check if your booking information is correct ?
             </h2>
 
->>>>>>> uptodate
+            {/* >>>>>>> uptodate */}
             <div className="py-2 px-3 ">
                 <CarInfoDetails>
                     name:{" "}
@@ -79,28 +57,7 @@ const CarDetailsFormFilled = ({ carDetails }) => {
                     Email: <UserDetailStyle>{carDetails.email}</UserDetailStyle>{" "}
                 </CarInfoDetails>
                 <CarInfoDetails>
-<<<<<<< HEAD
-                    contact:{" "}
-                    <UserDetailStyle>{carDetails.phone}</UserDetailStyle>
-                </CarInfoDetails>
-                <CarInfoDetails>
-                    State:{" "}
-                    <UserDetailStyle>
-                        {carDetails.stateOfOperation}
-                    </UserDetailStyle>
-                </CarInfoDetails>
-                <CarInfoDetails>
-                    Pickup location:{" "}
-                    <UserDetailStyle>
-                        {carDetails.pickUpLocation}
-                    </UserDetailStyle>
-                </CarInfoDetails>
-                <CarInfoDetails>
-                    DropOff location:
-                    <UserDetailStyle>
-                        {carDetails.dropOffLocation}
-                    </UserDetailStyle>
-=======
+
                     contact: <UserDetailStyle>{carDetails.phone}</UserDetailStyle>
                 </CarInfoDetails>
                 <CarInfoDetails>
@@ -114,7 +71,7 @@ const CarDetailsFormFilled = ({ carDetails }) => {
                 <CarInfoDetails>
                     DropOff location:
                     <UserDetailStyle>{carDetails.dropOffLocation}</UserDetailStyle>
->>>>>>> uptodate
+
                 </CarInfoDetails>
                 <CarInfoDetails>
                     Pickup Date:
@@ -125,25 +82,7 @@ const CarDetailsFormFilled = ({ carDetails }) => {
                     <UserDetailStyle>{carDetails.dropOffDate}</UserDetailStyle>
                 </CarInfoDetails>
             </div>
-<<<<<<< HEAD
 
-            <div className="flex p-2 gap-2">
-                <ItemStyle>
-                    <button className="booking_btn bg-green-600" type="button">
-                        book & pay now
-                    </button>
-                </ItemStyle>
-
-                <ItemStyle>
-                    <button onClick={submitForm} className="booking_btn" type="submit">
-                        book & pay later
-                    </button>
-                </ItemStyle>
-            </div>
-
-        </div>
-    );
-=======
 
             <div className="flex p-2 gap-2">
                 <ItemStyle>
@@ -161,7 +100,7 @@ const CarDetailsFormFilled = ({ carDetails }) => {
         </div>
     );
 
->>>>>>> uptodate
+
 };
 
 export default CarDetailsFormFilled;
