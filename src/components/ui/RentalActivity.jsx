bd20aff9df1528ea741373ff4cad32c4c4a7cfd7
ngwasidwan <sidwanche@gmail.com>
2024-09-02 06:33:35 +0100

    built dashboard

---
+++ src/components/ui/RentalActivity.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import ActivityList from "./ActivityList";
+
+function RentalActivity({ data }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  console.log(setActiveIndex);
+
+  return (
+    <div className="mx-auto w-11/12 py-10">
+      <h1 className=" text-green-700 font-bold mb-4 text-xl">
+        Rental activities
+      </h1>
+
+      <div className="grid grid-cols-[.3fr_1fr] gap-10">
+        <div>
+          {data.map((el, i) => (
+            <ActivityList
+              activity={el}
+              key={i}
+              setActiveIndex={setActiveIndex}
+              activeIndex={activeIndex}
+              index={i}
+            />
+          ))}
+        </div>
+
+        <div className="grid grid-cols-2 gap-10 ">
+          <ul className="flex flex-col gap-4">
+            <li>
+              <span>name of vehicle: {data[activeIndex].name}</span>
+            </li>
+
+            <li>
+              <span>price: ${data[activeIndex].price}</span>
+            </li>
+
+            <li>
+              <span>pick up Location: </span>
+              <span>{data[activeIndex].pickUpLocation}</span>
+            </li>
+            <li>
+              <span>drop off Location: </span>
+              <span>{data[activeIndex].dropOffLocation}</span>
+            </li>
+
+            <li>
+              <span>pick up date: </span>
+              <span>{data[activeIndex].pickUpDate}</span>
+            </li>
+
+            <li>
+              <span>drop off date</span>
+              <span>{data[activeIndex].DropOffDate}</span>
+            </li>
+          </ul>
+          <img src={data[activeIndex].image} alt="car" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RentalActivity;
--- src/lib/supabaseApi.js
+++ src/lib/supabaseApi.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-//go to ur .env file and add these VITE_SUPABASE_URL and VITE_SUPABASE_KEY.
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function getAllGuestReservations() {
